3d1b02851c94cee5896befcd250ffac8f38e34f9
aasuncion <a_asuncion@fanshaweonline.ca>
2022-09-30 00:54:57 -0400

    add scroll to top button

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,25 @@
         document.getElementById('spinner').style.display = 'none'
         document.getElementById('v-pills-tabContent').style.display = 'block';
     },1000)
-   
+    
+
+
 }) ();
 
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+let mybutton = document.getElementById("scrollToTop");
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+  
+  // When the user clicks on the button, scroll to the top of the document
+  function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
